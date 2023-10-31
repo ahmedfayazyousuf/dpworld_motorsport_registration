a818ca804af340f41757c737496ca15cdf04196e
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2023-10-31 18:10:38 +0400

    p
    p

--- src/Components/Success/index.js
+++ src/Components/Success/index.js
@@ -1,13 +1,58 @@
-import DPWorldLogo from '../1_Assets/DPWorldLogo.png'
+import React, { useEffect, useState } from 'react';
+import DPWorldLogo from '../1_Assets/DPWorldLogo.png';
+import firebase from '../../firebase';
 
 const Success = () => {
-    return(
-        <div style={{display:"flex", flexDirection:"column", width:"100%", height: "100vh", justifyContent:"center", alignItems:"center", flexWrap: 'wrap', textAlign: 'center'}}>
-            <img style={{minWidth: '100px', maxWidth: '300px'}} src={DPWorldLogo} alt="tick"/>
-            <h1 style={{paddingRight: '50px', paddingLeft: '50px', color: 'white', fontSize: '20px'}}>Your response has been recorded</h1>
-            <h1 style={{paddingRight: '50px', paddingLeft: '50px', color: 'white', marginTop: '10px'}}>LET'S PLAY!</h1>
+  const [player1Name, setPlayer1Name] = useState("Loading...");
+  const [player2Name, setPlayer2Name] = useState("Loading...");
+
+  useEffect(() => {
+    const CurrentUsers = firebase.firestore().collection("CurrentUsers");
+
+    // Set up a listener to fetch the data from "CurrentUsers" collection in real-time
+    const unsubscribe = CurrentUsers.onSnapshot((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const playerData = doc.data();
+        if (playerData.Player === "P1") {
+          setPlayer1Name(playerData.Name);
+        } else if (playerData.Player === "P2") {
+          setPlayer2Name(playerData.Name);
+        }
+      });
+    });
+
+    // Return a cleanup function to unsubscribe from the snapshot listener when the component unmounts
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column", width: "100%", height: "100vh", justifyContent: "center", alignItems: "center", flexWrap: 'wrap', textAlign: 'center' }}>
+        <img style={{ minWidth: '100px', maxWidth: '300px' }} src={DPWorldLogo} alt="tick" />
+        <h1 style={{ paddingRight: '50px', paddingLeft: '50px', color: 'white', fontSize: '20px' }}>Your response has been recorded</h1>
+        <h1 style={{ paddingRight: '50px', paddingLeft: '50px', color: 'white', marginTop: '10px' }}>LET'S PLAY!</h1>
+
+        <div style={{ display: 'flex', width: '280px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <div style={{display: 'flex', width: '120px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <p style={{ color: 'white' }}>Player 1: </p>
+            </div>
+            <div style={{display: 'flex', width: '160px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <p style={{ color: 'white' }}>{player1Name}</p>
+            </div>
         </div>
-    )
+
+        <div style={{ display: 'flex', width: '280px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <div style={{display: 'flex', width: '120px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <p style={{ color: 'white' }}>Player 2: </p>
+            </div>
+            <div style={{display: 'flex', width: '160px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <p style={{ color: 'white' }}>{player2Name}</p>
+            </div>
+        </div>
+        
+    </div>
+  )
 }
 
 export default Success;
